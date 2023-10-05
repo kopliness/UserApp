d3eb8311ee622c82efb3d62fe3c9f09b17c41d53
Kopliness <mr.vladlis3568@gmail.com>
2023-10-05 19:28:51 +0300

    added entities

---
+++ UserApp.DAL/Entities/Role.cs
@@ -0,0 +1,8 @@
+namespace DataLayer.Entities;
+
+public class Role
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; }
+}
---
+++ UserApp.DAL/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace DataLayer.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; }
+}
